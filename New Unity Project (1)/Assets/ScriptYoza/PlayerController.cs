5bf5f5327b0ab613571784487aff07fc47634912
yoza-yuya <yuuya-yoza-20f@stu.kbc.ac.jp>
2023-01-12 13:46:37 +0900

    プレイヤーの操作実装とInputManagerの設定

---
+++ New Unity Project (1)/Assets/ScriptYoza/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 3f;
+	float moveX = 0f;
+	float moveZ = 0f;
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		moveX = Input.GetAxis("Horizontal") * speed;
+		moveZ = Input.GetAxis("Vertical") * -speed;
+		Vector3 direction = new Vector3(moveX, 0, moveZ);
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector3(moveX, 0, moveZ);
+	}
+}
---
+++ New Unity Project (1)/Assets/ScriptYoza/RandomMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    public float speed = 3.5f;
+    private float chargeTime = 5.0f;
+    private float timeCount;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+
+
+        // 指定時間の経過（条件）
+        if (timeCount > chargeTime)
+        {
+            // 進路をランダムに変更する
+            Vector3 course = new Vector3(0, Random.Range(0, 180), 0);
+            transform.localRotation = Quaternion.Euler(course);
+
+            // タイムカウントを０に戻す
+            timeCount = 0;
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+    }
+}
